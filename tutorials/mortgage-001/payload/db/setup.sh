24a707ef950be473af94908249343bfc211532c2
Shankar Pentyala <shankar.pentyalaibm.com@shankars-mbp.usks.ibm.com>
2019-04-24 15:55:05 -0500

    mtutorial

--- tutorials/mortgage-001/payload/db/setup.sh
+++ tutorials/mortgage-001/payload/db/setup.sh
@@ -19,9 +19,11 @@ __loadDB2Docker()
                 exit 1
    fi
 
-   if [[ ! `docker images --quiet ibmcom/db2express-c:latest` ]]; then
+   if [[ ! `docker images --quiet store/ibmcorp/db2_developer_c:11.1.4.4-ppc64le` ]]; then
         echo -e "\nLoading Docker $DB2_DOCKER_NAME ..."
-        $D run -d --name $DB2_DOCKER_NAME -p 50000:50000 -e DB2INST1_PASSWORD=password -e LICENSE=accept  ibmcom/db2express-c:latest db2start
+       # $D run -d --name $DB2_DOCKER_NAME -p 50000:50000 -e DB2INST1_PASSWORD=password -e LICENSE=accept  ibmcom/db2express-c:latest db2start
+      docker run -h db2server_  --name $DB2_DOCKER_NAME --restart=always  --detach   --privileged=true -p 50000:50000 -p 55000:55000  --env-file .env_list   -v /root/db2cdata:/database store/ibmcorp/db2_developer_c:11.1.4.4-ppc64le
+
         #Wait couple of minutes to make sure db2 instance started and online
         sleep 120
    fi
